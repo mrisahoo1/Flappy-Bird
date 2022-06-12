351696f9acdca114af29c2918a227816b01fac84
Mrityunjay Sahoo <mrityunjaysahoo6@gmail.com>
2022-06-12 12:17:22 +0530

    Chnages in images

--- main.py
+++ main.py
@@ -18,6 +18,36 @@ PLAYER = 'gallery/sprites/bird.png'
 BACKGROUND = 'gallery/sprites/background.png'
 PIPE = 'gallery/sprites/pipe.png'
 
+
+def welcomeScreen():
+
+    playerx = int(SCREENWIDTH/5)
+    playery = int(SCREENHEIGHT - GAME_SPRITES['player'].get_height())/2
+    messagex = int((SCREENWIDTH - GAME_SPRITES['message'].get_width())/2)
+    messagey = int(SCREENHEIGHT*0.13)
+    
+    basex = 0
+    while True:
+        for event in pygame.event.get():
+            # if user clicks on the cross button, game over
+            if event.type == QUIT or (event.type==KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+            # if the user presses space or up key, start the game for them
+            elif event.type==KEYDOWN and (event.key==K_SPACE or event.key == K_UP):
+                return
+            else: 
+                SCREEN.blit(GAME_SPRITES['background'], (0, 0))
+                SCREEN.blit(GAME_SPRITES['player'], (playerx, playery))
+                SCREEN.blit(GAME_SPRITES['message'], (messagex, messagey))
+                SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))
+                pygame.display.update()
+                FPSCLOCK.tick(FPS)
+
+
+
+
 if __name__=="__main__":
     # this will be the main point where the game will start 
 
@@ -37,6 +67,7 @@ if __name__=="__main__":
         pygame.image.load('gallery/sprites/9.png').convert_alpha(),
     )
 
+    GAME_SPRITES['message'] =pygame.image.load('gallery/sprites/message.png').convert_alpha()
     GAME_SPRITES['base'] =pygame.image.load('gallery/sprites/base.png').convert_alpha()
     GAME_SPRITES['pipe'] =(pygame.transform.rotate(pygame.image.load( PIPE).convert_alpha(), 180), 
     pygame.image.load(PIPE).convert_alpha()
