f1800b53eadb25c85697647c1ac57837f462247b
Mrityunjay Sahoo <mrityunjaysahoo6@gmail.com>
2022-06-12 13:39:32 +0530

    Final changes made

--- main.py
+++ main.py
@@ -50,14 +50,13 @@ def mainGame():
     score = 0
     playerx = int(SCREENWIDTH/5)
     playery = int(SCREENWIDTH/2)
-    basex = 0 
+    basex = 0
 
-    # Creating 2 pipes for blitting the screen
-
+    # Create 2 pipes for blitting on the screen
     newPipe1 = getRandomPipe()
     newPipe2 = getRandomPipe()
 
-    #my list of upper pipes
+    # my List of upper pipes
     upperPipes = [
         {'x': SCREENWIDTH+200, 'y':newPipe1[0]['y']},
         {'x': SCREENWIDTH+200+(SCREENWIDTH/2), 'y':newPipe2[0]['y']},
@@ -70,13 +69,14 @@ def mainGame():
 
     pipeVelX = -4
 
-    playerVely = -9
-    playerMaxVelY = -10
+    playerVelY = -9
+    playerMaxVelY = 10
     playerMinVelY = -8
     playerAccY = 1
 
-    playerFlapAccv = -8  # velocity while flapping
-    PlayerFlapped = False   # It is true only when the bird is flapping
+    playerFlapAccv = -8 # velocity while flapping
+    playerFlapped = False # It is true only when the bird is flapping
+
 
     while True:
         for event in pygame.event.get():
@@ -86,13 +86,13 @@ def mainGame():
             if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
                 if playery > 0:
                     playerVelY = playerFlapAccv
-                    PlayerFlapped = True
+                    playerFlapped = True
                     GAME_SOUNDS['wing'].play()
 
 
-        crashTest = isCollide(playerx, playery, upperPipes, lowerPipes)  # This function will return true if the player is crashed
+        crashTest = isCollide(playerx, playery, upperPipes, lowerPipes) # This function will return true if the player is crashed
         if crashTest:
-            return
+            return     
 
         #check for score
         playerMidPos = playerx + GAME_SPRITES['player'].get_width()/2
@@ -100,19 +100,19 @@ def mainGame():
             pipeMidPos = pipe['x'] + GAME_SPRITES['pipe'][0].get_width()/2
             if pipeMidPos<= playerMidPos < pipeMidPos +4:
                 score +=1
-                print(f"Your score is {score}")
-            GAME_SOUNDS['point'].play()
+                print(f"Your score is {score}") 
+                GAME_SOUNDS['point'].play()
 
-        
-        if playerVelY <playerMaxVelY and not PlayerFlapped:
-            playerVelY+= playerAccY
 
-        if PlayerFlapped:
-            PlayerFlapped = False
-            playerHeight = GAME_SPRITES['player'].get_height()
-            playery = playery + min(playerVelY, GROUNDY - playery - playerHeight)
+        if playerVelY <playerMaxVelY and not playerFlapped:
+            playerVelY += playerAccY
 
-            # move pipes to the left
+        if playerFlapped:
+            playerFlapped = False            
+        playerHeight = GAME_SPRITES['player'].get_height()
+        playery = playery + min(playerVelY, GROUNDY - playery - playerHeight)
+
+        # move pipes to the left
         for upperPipe , lowerPipe in zip(upperPipes, lowerPipes):
             upperPipe['x'] += pipeVelX
             lowerPipe['x'] += pipeVelX
@@ -149,9 +149,23 @@ def mainGame():
         FPSCLOCK.tick(FPS)
 
 
+def isCollide(playerx, playery, upperPipes, lowerPipes):
+    if playery> GROUNDY - 25  or playery<0:
+        GAME_SOUNDS['hit'].play()
+        return True
+    
+    for pipe in upperPipes:
+        pipeHeight = GAME_SPRITES['pipe'][0].get_height()
+        if(playery < pipeHeight + pipe['y'] and abs(playerx - pipe['x']) < GAME_SPRITES['pipe'][0].get_width()):
+            GAME_SOUNDS['hit'].play()
+            return True
 
+    for pipe in lowerPipes:
+        if (playery + GAME_SPRITES['player'].get_height() > pipe['y']) and abs(playerx - pipe['x']) < GAME_SPRITES['pipe'][0].get_width():
+            GAME_SOUNDS['hit'].play()
+            return True
 
-
+    return False
 
 
 
@@ -167,12 +181,6 @@ def getRandomPipe():
         {'x': pipeX, 'y': y2} #lower Pipe
     ]
     return pipe
-
-
-
-
-
-
 
 
 
